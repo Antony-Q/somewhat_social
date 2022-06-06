c0ebcb9e48043e97a2b7af571379b9b736c044a1
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-06-05 23:45:47 -0500

    fixed user model

--- models/user.js
+++ models/user.js
@@ -1,55 +1,49 @@
 const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
 
-const PizzaSchema = new Schema(
-  {
-    pizzaName: {
-      type: String,
-      required: true,
-      trim: true
+const UserSchema = new Schema(
+    {
+    userName: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
     },
-    createdBy: {
-      type: String,
-      required: true,
-      trim: true
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+
     },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: createdAtVal => dateFormat(createdAtVal)
-    },
-    size: {
-      type: String,
-      required: true,
-      enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
-      default: 'Large'
-    },
-    toppings: [],
-    comments: [
-      {
+    thoughts: [
+        {
         type: Schema.Types.ObjectId,
-        ref: 'Comment'
-      }
+        ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+        type: Schema.Types.ObjectId,
+        }
     ]
-  },
-  {
-    toJSON: {
-      virtuals: true,
-      getters: true
     },
-    // prevents virtuals from creating duplicate of _id as `id`
-    id: false
-  }
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        // prevents virtuals from creating duplicate of _id as `id`
+        id: false
+    }
 );
 
-// get total count of comments and replies on retrieval
-PizzaSchema.virtual('commentCount').get(function() {
-  return this.comments.reduce(
-    (total, comment) => total + comment.replies.length + 1,
-    0
-  );
+// get total count of friends and replies on retrieval
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.reduce(
+        (total, friend) => total + friend.replies.length + 1,
+        0
+    );
 });
 
-const Pizza = model('Pizza', PizzaSchema);
+const User = model('User', UserSchema);
 
-module.exports = Pizza;
+module.exports = User;
