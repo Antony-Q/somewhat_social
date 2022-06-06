bf30dd54f39be06bc23141e5e04cd3d68d2d7589
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-06-06 00:38:22 -0500

    finished thought controller by adding crreate and delete function for reactions

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -74,6 +74,34 @@ const thoughtController = {
       })
       .catch(err => res.json(err));
     },
+
+        // create reaction
+        addReaction({ params, body }, res) {
+            Thought.findByIdAndUpdate(
+                { _id: params.thoughtId },
+                { $push: { reactions: body } },
+                { new: true }
+                )
+                .then(dbUserData => {
+                    if (!dbUserData) {
+                  res.status(404).json({ message: 'No user found with this id!' });
+                  return;
+                }
+                res.json(dbUserData);
+              })
+              .catch(err => res.json(err));
+        },
+        
+        // delete reaction
+        deleteReaction({ params }, res) {
+            Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: { reactionId: params.reactionId } } },
+            { new: true }
+        )
+          .then(dbUserData => res.json(dbUserData))
+          .catch(err => res.json(err));
+        },
 };
 
 module.exports = ThoughtController;
